17d93825bf104d6771ca26473164a995d3858f63
aggarwalanchit4 <aggarwalanchit4@gmail.com>
2026-08-03 23:38:48 +0530

    completed polymorphism

---
+++ Day-019/builtin_polymorphism.py
@@ -0,0 +1,21 @@
+# -----------------------------------------
+# Challenge 1 — Built-in Polymorphism
+# -----------------------------------------
+
+name = "Rahul"
+print(len(name))
+
+numbers = [10, 20, 30, 40]
+
+print(len(numbers))
+
+fruits = ("Apple", "Banana", "Mango")
+
+print(len(fruits))
+
+
+print(10 + 20)
+
+print("Hello " + "World")
+
+print([1, 2] + [3, 4])
--- Day-019/mini_project.py
+++ Day-019/mini_project.py
@@ -1,1 +1,31 @@
-Challenge 1 — Basic Polymorphism
+# -----------------------------------------
+# Day 19 Mini Project — Animal Sound Simulator
+# -----------------------------------------
+
+class Animal:
+    def speak(self):
+        print("Animal makes a sound")
+class Dog(Animal):
+    def speak(self):
+        print("DOG BARK's")
+class Cat(Animal):
+    def speak(self):
+        print("CAT MEOW's")
+class Lion(Animal):
+    def speak(self):
+        print("LION ROAR's")
+
+def animal_sound(animal):
+    animal.speak()
+
+animal1 = Animal()
+dog1 = Dog()
+cat1 = Cat()
+lion1 = Lion()
+
+
+animal_sound(animal1)
+animal_sound(dog1)
+animal_sound(cat1)
+animal_sound(lion1)
+
