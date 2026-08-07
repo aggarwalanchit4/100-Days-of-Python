b990cd0ea773cda02a891f1584571440f4f5585a
aggarwalanchit4 <aggarwalanchit4@gmail.com>
2026-08-07 23:52:29 +0530

    magic dunder basics

---
+++ Day-021/dunder_basics.py
@@ -0,0 +1,9 @@
+# -----------------------------------------
+# Challenge 1 – __str__()
+# -----------------------------------------
+ 
+class Book:
+    def __init__(self , name):
+        self.name = name
+    def __str__(self):
+        pass
